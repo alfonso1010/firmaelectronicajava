5b0a880f3f72d32d96384f40e471adc8afba8cfd
aarellanes <aarellanes@artificesweb.com>
2020-08-12 00:35:06 -0500

    se sube firma

--- src/main/java/io/leiva/app/controllers/SoapTitulosController.java
+++ src/main/java/io/leiva/app/controllers/SoapTitulosController.java
@@ -3,6 +3,8 @@ package io.leiva.app.controllers;
 import java.io.IOException;
 import java.security.PrivateKey;
 import java.security.Signature;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringJoiner;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.ssl.PKCS8Key;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.leiva.app.soap.SoapClientTitulos;
@@ -32,14 +36,14 @@ public class SoapTitulosController {
     
     
     @PostMapping("/generarFirma") // //new annotation since 4.3
-    public String singleFileUpload (@RequestParam("files") MultipartFile[] files,HttpServletRequest request,
+    public Map<String, String> singleFileUpload (@RequestParam("files") MultipartFile[] files,HttpServletRequest request,
             RedirectAttributes redirectAttributes) throws Exception {
     	byte[] archivo_cer = null;
     	byte[] archivo_key = null;
     	String password = request.getParameter("password");
     	String cadena_original = request.getParameter("cadena_original");
     	
-    	
+    	HashMap<String, String> map = new HashMap<>();
 		StringJoiner sj = new StringJoiner(" , ");
 		
 		for (MultipartFile file : files) {
@@ -50,23 +54,22 @@ public class SoapTitulosController {
 				String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
 				if(extension.equals(".cer")) {
 					archivo_cer = file.getBytes();
+					map.put("nombre_archivo_cer", file.getOriginalFilename());
 				}else if(extension.equals(".key")) {
 					archivo_key = file.getBytes();
+					map.put("nombre_archivo_key", file.getOriginalFilename());
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		StringBuffer result = new StringBuffer();
-		result.append("Archivo CER (base 64):\n");
-		result.append(Base64.encodeBase64String(archivo_cer));
-		result.append("\n");
-		result.append("\n");
-		result.append("Firma (base 64):\n");
-		result.append(
-				sign(archivo_key,password,cadena_original)
-				);
-		return result.toString();
+		try {
+			
+		    map.put("firma",sign(archivo_key,password,cadena_original));
+		    return map;
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Ocurrió un error generando la firma");
+		}
     }
     
     public static String sign(byte[] key, String password, String toSign) throws Exception {
